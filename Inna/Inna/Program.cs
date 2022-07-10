b11926b3c5d05531f74461bc2b6452bf8ecc0c41
InnaHorda <inna.horda@gmail.com>
2022-07-10 20:23:55 +0300

    new

---
+++ Inna/Inna/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Inna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
